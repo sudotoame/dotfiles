017c099b053053501c0e7ff2b858b9b79487a9fa
yusspetr <akaisuki2@gmail.com>
2026-03-26 15:20:15 +0900

    add/fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias l 'eza --icons'
+    alias ll 'eza --icons -l'
+    alias la 'eza --icons -la'
+    alias v nvim
+end
