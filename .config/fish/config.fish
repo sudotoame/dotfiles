1d311bd2522d6165bee27d5f30488b80779c7854
yusspetr <akaisuki2@gmail.com>
2026-03-26 20:14:30 +0900

    - update fish settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias l 'eza --icons'
+    alias ls 'eza --icons'
     alias ll 'eza --icons -l'
     alias la 'eza --icons -la'
+    alias gis 'git status'
+    alias gia 'git add'
+    alias gic 'git commit'
+    alias gip 'git push'
+    alias pls 'sudo dnf'
     alias v nvim
 end
