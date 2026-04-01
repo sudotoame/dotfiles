68c3bac4c9e4cd76764682f817eca58edf241e00
yusspetr <akaisuki2@gmail.com>
2026-04-01 18:47:06 +0900

    upd/fish
    add alias for ls, zoxide init for fish, cat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -g fish_greeting ""
     alias ls 'eza --icons'
-    alias ll 'eza --icons -l'
-    alias la 'eza --icons -la'
+    alias l 'ls -l'
+    alias la 'ls -a'
+    alias lla 'ls -la'
+    alias lt 'ls --tree'
     alias gis 'git status'
     alias gia 'git add'
     alias gic 'git commit'
     alias gip 'git push'
     alias pls 'sudo dnf'
-    alias v nvim
+    alias v 'nvim'
+    alias cat 'bat'
+    zoxide init fish | source
 end
